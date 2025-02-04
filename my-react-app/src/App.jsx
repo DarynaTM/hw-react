30055c0bdb510ef7dd6d553572facd0b2ac3803f
Daryna Tymoshenko <drn.tymoshenko@gmail.com>
2025-02-04 23:51:25 +0800

    Creating react project and adding react cards

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Card from "./Card";
 
-function App() {
-  const [count, setCount] = useState(0)
+const data = [
+  {
+    img: "https://uploads5.wikiart.org/images/maria-prymachenko/ivan-gave-the-landlord-a-ride-in-his-gig-and-fell-inside-1983.jpg!Large.jpg",
+    title: "Отак Іван пана покатав, аж у коляску впав",
+    description: "Рік: 1983. Жанр: жанровий живопис",
+  },
+  {
+    img: "https://uploads2.wikiart.org/images/maria-prymachenko/this-beast-is-making-magic-1983.jpg!Large.jpg",
+    title: "Цей звір ворожить",
+    description: "Рік: 1983. Жанр: міфологічний живопис",
+  },
+  {
+    img: "https://uploads0.wikiart.org/images/maria-prymachenko/monkey-riding-a-four-headed-beast-1982.jpg!Large.jpg",
+    title: "Мавпа їде на чотириголовому звірі",
+    description: "Рік: 1982. Жанр: міфологічний живопис",
+  },
+];
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="container">
+      {data.map((item, index) => (
+        <Card key={index} img={item.img} title={item.title} description={item.description} />
+      ))}
+    </div>
+  );
+};
 
-export default App
+export default App;
